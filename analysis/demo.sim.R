f5f086dc3438368be6b30b8e98185f23e67200cb
Peter Carbonetto <peter.carbonetto@gmail.com>
2017-08-27 15:17:29 -0500

    Added varbvs results to demo.sim.R.

--- analysis/demo.sim.R
+++ analysis/demo.sim.R
@@ -1,5 +1,6 @@
 # TO DO: Explain what this script does, and how to use it.
-
+library(ggplot2)
+library(cowplot)
 library(varbvs)
 
 # SCRIPT PARAMETERS
@@ -18,9 +19,10 @@ set.seed(1)
 
 # GENERATE DATA SET
 # -----------------
-# Generate the minor allele frequencies so that they are uniform over range
-# [0.05,0.5]. Then simulate genotypes assuming all markers are uncorrelated
-# (i.e., unlinked), according to the specified minor allele frequencies.
+# Generate the minor allele frequencies so that they are uniform over
+# range [0.05,0.5]. Then simulate genotypes assuming all markers are
+# uncorrelated (i.e., unlinked), according to the minor allele
+# frequencies specified by vector "maf".
 cat("1. GENERATING DATA SET.\n")
 maf <- 0.05 + 0.45*runif(p)
 X   <- (runif(n*p) < maf) +
@@ -49,3 +51,46 @@ mu <- rnorm(1)
 # Generate the quantitative trait measurements.
 y <- c(mu + X %*% beta + sqrt(se)*rnorm(n))
 
+# FIT VARIATIONAL APPROXIMATION TO POSTERIOR
+# ------------------------------------------
+# Fit the fully-factorized variational approximation to the posterior
+# distribution of the coefficients for a linear regression model of a
+# continuous outcome (quantitiative trait), with spike and slab priors on
+# the coefficients. 
+cat("2. FITTING VARBVS MODEL TO DATA.\n")
+fit.varbvs <- varbvs(X,NULL,y,"gaussian",logodds = logodds,n0 = 0)
+cat("\n")
+
+cat("3. FITTING BSLMM MODEL TO DATA.\n")
+# TO DO.
+
+# TO DO: Add comments here explaining what this code chunk does.
+cat("4. SUMMARIZING VARBVS & BSLMM RESULTS.\n")
+
+# Compute the false positive rate (FPR) and true positive rate (TPR)
+# at each PIP threshold.
+dat <- data.frame(pip.cutoff = seq(0,1,0.01),
+                  tpr.varbvs = 0,
+                  fpr.varbvs = 0,
+                  tpr.bslmm  = 0,
+                  fpr.bslmm  = 0)
+for (i in 1:101) {
+
+  # Get the PIP threshold.
+  r <- dat$pip.cutoff[i]
+
+  # Compute the varbvs TPR and FPR.
+  np                <- sum(fit.varbvs$pip >= r)
+  ntp               <- sum(fit.varbvs$pip >= r & beta != 0)
+  nfp               <- sum(fit.varbvs$pip >= r & beta == 0)
+  dat$tpr.varbvs[i] <- ntp / np
+  dat$fpr.varbvs[i] <- nfp / np
+}
+
+p1 <- ggplot(dat,aes(x = pip.cutoff,y = tpr.varbvs)) +
+  geom_line()
+
+p2 <- ggplot(dat,aes(x = pip.cutoff,y = fpr.varbvs)) +
+  geom_line()
+
+print(plot_grid(p1,p2))
